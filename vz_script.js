2b6434b3e0f3e1623be1b9a9c2e805994c13b423
Vatsal Joshi <vatz88@gmail.com>
2016-08-21 13:18:11 +0530

    Meta data added

--- vz_script.js
+++ vz_script.js
@@ -1,6 +1,6 @@
 //Add content
 var content_about_me =
-    
+
     "I'm VaTz88. Engg stud at VIT.";
 
 var content_connect_with_me =
@@ -14,16 +14,15 @@ $("#ConnectWithMeContent").text(content_connect_with_me);//--Add content
 $(".collapsible-header").addClass("waves-effect waves-teal");//--Add waves effect to collapsible-header
 
 //Disable mouse right click
-document.oncontextmenu = function(){
+document.oncontextmenu = function () {
     return false;
 };//--Disable mouse right click
 
 //Dis able keyboard keys crtl+u and ctrl+shift+i
-document.onkeydown = function(e){
-    if(e.ctrlKey && ((e.keyCode === 117 || e.keyCode === 85) || (e.shiftKey && (e.keyCode === 73 || e.keyCode === 105)))){
+document.onkeydown = function (e) {
+    if (e.ctrlKey && ((e.keyCode === 117 || e.keyCode === 85) || (e.shiftKey && (e.keyCode === 73 || e.keyCode === 105)))) {
         return false;
-    }else{
+    } else {
         return true;
     }
-};
-//--Dis able keyboard keys crtl+u and ctrl+shift+i
+};//--Dis able keyboard keys crtl+u and ctrl+shift+i

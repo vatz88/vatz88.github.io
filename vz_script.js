5678735500dd7eb1f57b8d72893b9b0db1d076fd
VaTz88 <vatz88@gmail.com>
2017-01-16 01:33:41 +0530

    better https redirect

--- vz_script.js
+++ vz_script.js
@@ -1,1 +1,17 @@
-$(".collapsible-header").addClass("waves-effect waves-teal");$(document).ready(function(){$(".modal-trigger").leanModal();if(window.location.href.indexOf("#resume")!=-1){$("#resume").openModal()}});document.oncontextmenu=function(){return false};document.onkeydown=function(b){if(b.ctrlKey&&((b.keyCode===117||b.keyCode===85)||(b.shiftKey&&(b.keyCode===73||b.keyCode===105)))){return false}else{return true}};
+$(".collapsible-header").addClass("waves-effect waves-teal");
+$(document).ready(function() {
+    $(".modal-trigger").leanModal();
+    if (window.location.href.indexOf("#resume") != -1) {
+        $("#resume").openModal()
+    }
+});
+document.oncontextmenu = function() {
+    return false
+};
+document.onkeydown = function(b) {
+    if (b.ctrlKey && ((b.keyCode === 117 || b.keyCode === 85) || (b.shiftKey && (b.keyCode === 73 || b.keyCode === 105)))) {
+        return false
+    } else {
+        return true
+    }
+};

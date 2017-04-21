f0ea75fa542bcb2f679544edc51409075bfbdd45
Vatsal Joshi <vatz88@gmail.com>
2017-04-21 13:20:26 +0530

    pic follow cursor

--- js/vz_script.js
+++ js/vz_script.js
@@ -33,10 +33,49 @@ document.oncontextmenu = function (e) {
     }
 };
 
+var onkeydownT = true;
 document.onkeydown = function (e) {
-    if (e.ctrlKey && ((e.keyCode === 117 || e.keyCode === 85) || (e.shiftKey && (e.keyCode === 73 || e.keyCode === 105)))) {
+    if (e.ctrlKey && ((e.keyCode === 117 || e.keyCode === 85) || (e.shiftKey && ((e.keyCode === 73 || e.keyCode === 105) || (e.keyCode === 67 || e.keyCode === 99))))) {
+        if (onkeydownT) {
+            onkeydownT = false;
+            Materialize.toast('<h3>Loved it?</h3>', 2500, 'rounded', function () {
+                Materialize.toast('<h3>Interested in code?</h3>', 2500, 'rounded', function () {
+                    Materialize.toast('<h3>Follow me on GitHub :)</h3>', 3000, 'rounded', function () {
+                        onkeydownT = true;
+                    });
+                });
+            });
+        }
         return false;
     } else {
         return true;
     }
 };
+
+/* I'm is watching you! */
+var isTouchDevice = 'ontouchstart' in document.documentElement;
+if (!isTouchDevice) {
+    var $profilePic = $('#profilePic');
+    var dh = $(document).height() / 2;
+    var dw = $(document).width() / 2;
+    var maxRotate = 20;
+    $(document).mousemove(function (event) {
+        var Xdeg = 90 - (event.pageY * 90) / dh;
+        var Ydeg = -90 + (event.pageX * 90) / dw;
+        if (Xdeg > 0) {
+            Xdeg = Xdeg > maxRotate ? maxRotate : Xdeg % maxRotate;
+        } else {
+            Xdeg = Xdeg < -maxRotate ? -maxRotate : -(-Xdeg % maxRotate);
+        }
+        if (Ydeg > 0) {
+            Ydeg = Ydeg > maxRotate ? maxRotate : Ydeg % maxRotate;
+        } else {
+            Ydeg = Ydeg < -maxRotate ? -maxRotate : -(-Ydeg % maxRotate);
+        }
+        $profilePic.css('transform', 'translateZ( -200px ) perspective( 600px ) rotateY( ' + Ydeg + 'deg ) rotateX( ' + Xdeg + 'deg )');
+    });
+}
+
+// console message
+var consolestyles = ['font-size: 46px', 'color: #757575'].join(';');
+console.log('%cVaTz88', consolestyles);

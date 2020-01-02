a1dadd1464a7a76ad2d80111135146165fe801b6
Vatsal Joshi <vatz88@gmail.com>
2020-01-02 18:34:18 +0530

    :sparkles: ripple effect on photo, onclick theme change :lipstick:

--- TODO.md
+++ TODO.md
@@ -1,6 +1,6 @@
 # TODO
 
-- Theme change on profile pic click (Light, Dark and may be more in future)
 - profile pic rotate on mouse move
 - Add more social links
 - Make new `favicon.ico` in the root.
+- More themes
--- src/components/Header.svelte
+++ src/components/Header.svelte
@@ -10,18 +10,42 @@
 
 <style>
   #profile-pic {
-    cursor: pointer;
+    height: 150px;
     width: 150px;
-    height: auto;
+  }
+
+  .ripple-ring {
+    cursor: pointer;
+    position: relative;
+  }
+
+  .ripple-ring::after {
+    /* ripple animation for 2s runs 2 times */
+    animation: ripple 2s linear 2;
+    /* animation starts after a delay of 3s */
+    animation-delay: 3s;
+    border-radius: 50%;
+    content: '';
+    height: 150px;
+    left: 0;
+    position: absolute;
+    width: 150px;
+  }
+
+  @keyframes ripple {
+    0% {
+      border: 1px solid rgba(#dbdce0, 1);
+      transform: scale(1);
+    }
+    100% {
+      border: 1px solid rgba(#dbdce0, 0);
+      transform: scale(1.1);
+    }
   }
 </style>
 
-<div class="fx-item fx-grow-zero">
-  <img
-    on:click={toggleTheme}
-    id="profile-pic"
-    src="/public/vatsal-joshi.png"
-    alt="Vatsal Joshi" />
+<div class="fx-item fx-grow-zero ripple-ring" on:click={toggleTheme}>
+  <img id="profile-pic" src="/public/vatsal-joshi.png" alt="Vatsal Joshi" />
 </div>
 <div class="fx-item fx-grow-zero">
   <h1>Vatsal Joshi</h1>
